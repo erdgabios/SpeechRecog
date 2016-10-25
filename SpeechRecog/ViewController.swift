411daa1b3903a5230a7faae489cad6e13282b3c9
Gabriel Erdelyi <erdgabios@gmail.com>
2016-10-25 12:43:34 +0200

    first commit

--- SpeechRecog/ViewController.swift
+++ SpeechRecog/ViewController.swift
@@ -1,25 +1,63 @@
 //
 //  ViewController.swift
-//  SpeechRecog
+//  Scribe
 //
-//  Created by G on 25/10/2016.
-//  Copyright © 2016 erdgabios. All rights reserved.
+//  Created by Caleb Stultz on 8/10/16.
+//  Copyright © 2016 Caleb Stultz. All rights reserved.
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var recordButton: UIButton!
+    var audioPlayer: AVAudioPlayer!
+    
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var transcriptionTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        activitySpinner.isHidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playBtnIsPressed(_ sender: AnyObject) {
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
     }
-
-
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activitySpinner.stopAnimating()
+        activitySpinner.isHidden = true
+    }
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                    } catch {
+                        print("Error!")
+                    }
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                            print("There was an error: \(error)")
+                        } else {
+                            self.transcriptionTextField.text = result?.bestTranscription.formattedString
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
